11c5534b4aad0a9ddd1ae214a7743ce4e21ee191
SafakAtan <safakatan0571@gmail.com>
2024-10-21 14:19:19 +0300

    Commets Fixed

--- client/client.cpp
+++ client/client.cpp
@@ -1,42 +1,73 @@
-// client.cpp
 #include <iostream>
+#include <cstring>
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <unistd.h>
-#include <cstring>
 #include <netdb.h>
+#include <thread>
+
+void receive_messages(int sock) {
+    char buffer[1024];
+    while (true) {
+        memset(buffer, 0, sizeof(buffer));
+        int valread = read(sock, buffer, sizeof(buffer));
+        if (valread > 0) {
+            std::cout << "Message from server: " << buffer << std::endl;
+        } else {
+            std::cerr << "Connection closed or read error." << std::endl;
+            break;
+        }
+    }
+}
 
 int main() {
     int sock = 0;
     struct sockaddr_in serv_addr;
-    struct hostent* server;
+    struct addrinfo hints, *res;
+    
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
 
-    // Sunucu adı üzerinden DNS çözümleme (server, docker-compose.yml'deki sunucu ismi)
-    server = gethostbyname("server");
-    if (server == NULL) {
-        std::cerr << "Sunucu bulunamadı" << std::endl;
+    // Resolve the server address
+    int status = getaddrinfo("server", "8080", &hints, &res);
+    if (status != 0) {
+        std::cerr << "getaddrinfo error: " << gai_strerror(status) << std::endl;
         return -1;
     }
 
-    // Socket oluştur
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        std::cerr << "Socket oluşturulamadı" << std::endl;
+    // Create socket
+    if ((sock = socket(res->ai_family, res->ai_socktype, res->ai_protocol)) < 0) {
+        std::cerr << "Socket creation failed." << std::endl;
         return -1;
     }
 
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(8080);
-    std::memcpy(&serv_addr.sin_addr.s_addr, server->h_addr, server->h_length);
-
-    // Sunucuya bağlan
-    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
-        std::cerr << "Bağlantı başarısız" << std::endl;
+    // Connect to the server
+    if (connect(sock, res->ai_addr, res->ai_addrlen) < 0) {
+        std::cerr << "Connection error." << std::endl;
         return -1;
     }
 
-    char buffer[1024] = {0};
-    read(sock, buffer, 1024);
-    std::cout << "Sunucudan mesaj: " << buffer << std::endl;
+    std::cout << "Connected to the server!" << std::endl;
+
+    freeaddrinfo(res);
+
+    // Create a thread for receiving messages
+    std::thread receiver(receive_messages, sock);
+    receiver.detach(); // Run the thread separately
+
+    // Message sending loop
+    while (true) {
+        std::string message;
+        std::cout << "Enter your message (type 'exit' to quit): ";
+        std::getline(std::cin, message);
+
+        if (message == "exit") {
+            break;
+        }
+
+        send(sock, message.c_str(), message.size(), 0);
+    }
 
     close(sock);
     return 0;
--- server/server.cpp
+++ server/server.cpp
@@ -1,35 +1,84 @@
-// server.cpp
 #include <iostream>
+#include <cstring>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <unistd.h>
-#include <cstring>
+#include <vector>
+#include <algorithm>
+
+std::vector<int> clients; // Vector to hold client sockets
+
+void handle_client(int client_socket) {
+    char buffer[1024];
+    int valread;
+
+    while ((valread = read(client_socket, buffer, sizeof(buffer))) > 0) {
+        buffer[valread] = '\0'; // Null terminate the buffer
+        std::cout << "Message received: " << buffer << std::endl;
+
+        // Send the message to all clients
+        for (int client : clients) {
+            if (client != client_socket) { // Do not send to the sender
+                send(client, buffer, strlen(buffer), 0);
+            }
+        }
+    }
+
+    // Remove the client from the list when done
+    std::cout << "Client disconnected." << std::endl;
+    clients.erase(std::remove(clients.begin(), clients.end(), client_socket), clients.end());
+    close(client_socket);
+}
+
 int main() {
     int server_fd, new_socket;
     struct sockaddr_in address;
     int opt = 1;
     int addrlen = sizeof(address);
 
-    // Socket file descriptor
-    server_fd = socket(AF_INET, SOCK_STREAM, 0);
-    
-    // Forcefully attaching socket to the port 8080
-    setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt));
+    // Create socket file descriptor
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        std::cerr << "Socket creation failed." << std::endl;
+        return -1;
+    }
+
+    // Attach socket to the port
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) {
+        std::cerr << "Set socket options failed." << std::endl;
+        return -1;
+    }
 
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(8080);
 
-    bind(server_fd, (struct sockaddr *)&address, sizeof(address));
-    listen(server_fd, 3);
+    // Bind the socket to the address
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+        std::cerr << "Binding failed." << std::endl;
+        return -1;
+    }
 
-    new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen);
-    
-    const char *hello = "Hello from server";
-    send(new_socket, hello, strlen(hello), 0);
-    std::cout << "Hello message sent\n";
-    
-    close(new_socket);
-    close(server_fd);
+    // Listen for incoming connections
+    if (listen(server_fd, 3) < 0) {
+        std::cerr << "Listening failed." << std::endl;
+        return -1;
+    }
+
+    std::cout << "Server is listening on port 8080..." << std::endl;
+
+    while (true) {
+        // Accept a new connection
+        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
+            std::cerr << "Accept failed." << std::endl;
+            return -1;
+        }
+
+        std::cout << "New client connected." << std::endl;
+        clients.push_back(new_socket); // Add new client to the list
+
+        // Create a new thread to handle the client
+        std::thread(handle_client, new_socket).detach();
+    }
+
     return 0;
 }
